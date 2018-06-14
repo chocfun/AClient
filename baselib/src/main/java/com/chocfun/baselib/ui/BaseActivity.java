28f3307330ebe36bcf85bf26f525c9591a1fd8aa
chocfun <25099591@qq.com>
2018-06-14 14:15:21 +0800

    -- upload files

---
+++ baselib/src/main/java/com/chocfun/baselib/ui/BaseActivity.java
@@ -0,0 +1,47 @@
+package com.chocfun.baselib.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+/**
+ * 封装Activity基类
+ *
+ */
+public abstract class BaseActivity extends AppCompatActivity {
+
+    private Unbinder mUnbinder;
+
+    /**
+     * 在这里设置Activity的View对应的layout文件
+     */
+    public abstract int getLayoutId();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        try {
+            int layoutId = getLayoutId();
+            if (layoutId > 0) {
+                setContentView(layoutId);
+
+                mUnbinder = ButterKnife.bind(this);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (null != mUnbinder) {
+            mUnbinder.unbind();
+        }
+    }
+}
---
+++ imageloadercomponent/src/main/java/com/chocfun/aclient/imageloadercomponent/ui/ImageActivity.java
@@ -0,0 +1,11 @@
+package com.chocfun.aclient.imageloadercomponent.ui;
+
+import com.chocfun.aclient.imageloadercomponent.R;
+import com.chocfun.baselib.ui.BaseActivity;
+
+public class ImageActivity extends BaseActivity {
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_image;
+    }
+}

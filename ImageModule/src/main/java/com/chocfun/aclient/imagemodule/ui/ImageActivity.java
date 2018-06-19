6c50c409d1bc40618dde2b72ec8c719e2b178b03
chocfun <25099591@qq.com>
2018-06-19 18:15:43 +0800

    -- 备份

--- ImageModule/src/main/java/com/chocfun/aclient/imagemodule/ui/ImageActivity.java
+++ ImageModule/src/main/java/com/chocfun/aclient/imagemodule/ui/ImageActivity.java
@@ -1,11 +1,19 @@
 package com.chocfun.aclient.imagemodule.ui;
 
+import android.widget.Spinner;
+
 import com.chocfun.aclient.imagemodule.R;
 import com.chocfun.baselib.ui.BaseActivity;
 
+import butterknife.BindView;
+
 public class ImageActivity extends BaseActivity {
+
+    @BindView(R.id.src_spinner)
+    Spinner mSrcSpinner;
+
     @Override
     public int getLayoutId() {
-        return R.layout.activity_image;
+        return R.layout.img_activity_image;
     }
 }
